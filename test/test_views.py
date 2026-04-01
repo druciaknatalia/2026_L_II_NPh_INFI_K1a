cfd029dc47577d79f79fd3d3fb624490d4ede216
druciaknatalia <natalia.druciak01@gmail.com>
2026-04-01 21:44:03 +0200

    Poprawa testu JSON

--- test/test_views.py
+++ test/test_views.py
@@ -1,18 +1,20 @@
 import unittest
+import json
 from hello_world import app
-from hello_world.formater import SUPPORTED
 
 
-class FlaskrTestCase(unittest.TestCase):
+class FlaskTestCase(unittest.TestCase):
+
     def setUp(self):
-        app.config['TESTING'] = True
         self.app = app.test_client()
-
-    def test_outputs(self):
-        rv = self.app.get('/outputs')
-        s = str(rv.data)
-        ','.join(SUPPORTED) in s
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Natalia", "mgs":Hello World!"}', rv.data)
+        self.assertEqual(
+            {"imie": "NataliaD", "mgs": "Hello World!"},
+            json.loads(rv.data)
+        )
+
+    def test_outputs(self):
+        rv = self.app.get('/outputs')
+        self.assertIn(b'plain', rv.data)
